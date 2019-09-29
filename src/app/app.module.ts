617266c4c42191f033faa65581aa3836d4ed2483
Nomokonov <INomokonov@luxoft.com>
2019-09-30 00:22:43 +0300

    data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 // import the PivotViewModule for the pivot table component
 import { PivotViewAllModule, PivotFieldListAllModule } from '@syncfusion/ej2-angular-pivotview';
 import { AppComponent }  from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @NgModule({
   //declaration of ej2-angular-pivotview module into NgModule
-  imports:      [ BrowserModule, PivotViewAllModule, PivotFieldListAllModule ],
+  imports:      [ BrowserModule, PivotViewAllModule, PivotFieldListAllModule, HttpClientModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [HttpClient]
 })
 export class AppModule { }
