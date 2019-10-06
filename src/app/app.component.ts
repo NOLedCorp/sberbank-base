4ae61d77e7a41a47407cd4eff0e864b941968862
NOLedCorp <noledcorp@gmail.com>
2019-10-07 00:27:44 +0300

    Интеграция с двумя вариантами вывода. Сделано очень итересно, можно выводить иерархично, можно без неё

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,8 +18,8 @@ export class AppComponent {
   public dataSourceSettings: IDataOptions;
   public gridSettings: GridSettings;
   
-  data:IDataSet[] = [];
-
+  data:any;
+  obj:any;
   report: any;
 
   constructor(private _http: HttpClient){}
@@ -27,32 +27,92 @@ export class AppComponent {
   getPivotData(): void {
       this._http.get<any>("../../assets/data.json").subscribe(data => {
         this.data = data;
-        this.dataSourceSettings = {
-          dataSource: this.data,
-          expandAll: false,
-          enableSorting: true,
-          columns: [{ name: 'Year', caption: 'Production Year' }, { name: 'Quarter' }],
-          values: [{ name: 'Sold', caption: 'Units Sold' }, { name: 'Amount', caption: 'Sold Amount' }],
-          rows: [{ name: 'Country' }, { name: 'Products' }],
-          formatSettings: [{ name: 'Amount', format: 'C0' }],
-          filters: [],
-          //calculatedFieldSettings: [{ name: 'Total', formula: "Sum(Amount)"+"Sum(Sold)"}]
-          };
+        // this.dataSourceSettings = {
+        //   dataSource: this.data,
+        //   expandAll: false,
+        //   enableSorting: true,
+        //   columns: [{ name: 'Year', caption: 'Production Year' }, { name: 'Quarter' }],
+        //   values: [{ name: 'Sold', caption: 'Units Sold' }, { name: 'Amount', caption: 'Sold Amount' }],
+        //   rows: [{ name: 'Country' }, { name: 'Products' }],
+        //   formatSettings: [{ name: 'Amount', format: 'C0' }],
+        //   filters: [],
+        //   //calculatedFieldSettings: [{ name: 'Total', formula: "Sum(Amount)"+"Sum(Sold)"}]
+        //   };
           
         //   this.editSettings = {
         //     allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Batch', allowCommandColumns: false,
         //     allowEditOnDblClick: true, showConfirmDialog: true, showDeleteConfirmDialog: false
         // } as CellEditSettings;
-          this.editSettings = {
-            allowAdding: true,
-            allowDeleting: true,
-            allowEditing: true,
-            mode: 'Batch'
-          } as CellEditSettings;
+          // this.editSettings = {
+          //   allowAdding: true,
+          //   allowDeleting: true,
+          //   allowEditing: true,
+          //   mode: 'Batch'
+          // } as CellEditSettings;
+
+          this.obj = [{
+            "Country": {
+              type: "level",
+              hierarchy: "Countries"
+            },
+            "Product": {
+              type: "level",
+              hierarchy: "Countries",
+              level: "Products",
+              parent: "Country"
+            },
+            "Year": {
+              type: "level",
+              hierarchy: "Years"
+            },
+            "Quarter": {
+              type: "level",
+              hierarchy: "Years",
+              level: "Quarters",
+              parent: "Year"
+            },
+            "Sold": {
+              type: "number"
+            },
+            "Amount": {
+              type: "number"
+            },
+          },
+          ...this.data
+        ]
 
           this.report = {
             dataSource: {
-              data: this.data
+              data: this.obj
+            },
+            slice: {
+              rows: [
+                {
+                  uniqueName: "Countries",
+                },
+                // {
+                //   uniqueName: "Product",
+                // },
+              ],
+              columns: [
+                {
+                  uniqueName: "Years"
+                },
+                // {
+                //   uniqueName: "Quarter"
+                // },
+                // {
+                //   uniqueName: "Measures"
+                // }
+              ],
+              measures: [
+                {
+                  uniqueName: "Sold"
+                },
+                {
+                  uniqueName: "Amount"
+                },
+              ]
             }
           }
       })
